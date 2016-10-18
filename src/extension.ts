2573dcda19bd97b12105bfca71e348c03cdbd28f
Nikita Gryzlov <nixel2007@gmail.com>
2016-10-18 12:47:36 +0300

    Установка sonarlint перенесена в сам extension.ts при активации расширения

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as fs from "fs";
+import * as path from 'path';
 import * as vscode from "vscode";
 import LintProvider from "./features/lintProvider";
+
+const https = require('follow-redirects').https;
+const extract = require('extract-zip')
+
+const pathToExtract = path.join(__filename, "./../../../tools");
+const pathToDownload = path.join(pathToExtract, "sonarlint-cli.zip");
 
 let diagnosticCollection: vscode.DiagnosticCollection;
 
@@ -9,6 +17,33 @@ let diagnosticCollection: vscode.DiagnosticCollection;
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+    if (!fs.existsSync(pathToDownload)) {
+        console.log("not found");
+        install(context);
+        return;
+    }
+
+    addSubscriptions(context);
+
+}
+
+function install(context: vscode.ExtensionContext) {
+    const URL = "https://github.com/nixel2007/sonarlint-cli/releases/download/console-report-1.0/sonarlint-cli.zip";
+    
+    return https.get(URL, (response) => {
+        response.pipe(fs.createWriteStream(pathToDownload)).on("finish", () => {
+            extract(pathToDownload, { dir: pathToExtract }, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                vscode.window.showInformationMessage("SonarLint was installed.");
+                addSubscriptions(context);
+            })
+        });
+    });
+}
+
+function addSubscriptions(context: vscode.ExtensionContext) {
     let linter = new LintProvider();
     linter.activate(context.subscriptions);
 
