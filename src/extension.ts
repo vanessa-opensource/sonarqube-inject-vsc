29da4c1eef55bdc53b4a822994bd46eabd75715e
Nikita Gryzlov <nixel2007@gmail.com>
2016-10-24 14:09:10 +0300

    Добавлены команды по созданию файлов настроек

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as fs from "async-file";
+import * as os from "os";
 import * as path from 'path';
 import * as vscode from "vscode";
+
+import { configTemplate } from "./templates/configTemplateInterface";
+import GlobalTemplate from "./templates/globalTemplate";
+import SonarlintTemplate from "./templates/sonarlintTemplate";
 import LintProvider from "./features/lintProvider";
 
 const https = require('follow-redirects').https;
@@ -30,7 +35,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
 function install(context: vscode.ExtensionContext) {
     const URL = "https://github.com/nixel2007/sonarlint-cli/releases/download/console-report-1.0/sonarlint-cli.zip";
-    
+
     return https.get(URL, (response) => {
         response.pipe(fs.createWriteStream(pathToDownload)).on("finish", () => {
             extract(pathToDownload, { dir: pathToExtract }, (err) => {
@@ -56,4 +61,67 @@ function addSubscriptions(context: vscode.ExtensionContext) {
         linter.updateBindings();
         linter.doLint();
     }));
+
+    context.subscriptions.push(vscode.commands.registerCommand("sonarqube-inject.createGlobalJson", createGlobalJson));
+
+    context.subscriptions.push(vscode.commands.registerCommand("sonarqube-inject.createSonarlintJson", createSonarlintJson));
+
+}
+
+async function createGlobalJson() {
+    const rootPath = path.join(os.homedir(), ".sonarlint");
+
+    if (!rootPath) {
+        vscode.window.showInformationMessage("SonarLint binary is not installed.")
+        return;
+    }
+
+    const confPath = path.join(rootPath, "conf");
+    try {
+        await fs.stat(confPath);
+    } catch (error) {
+        try {
+            await fs.mkdir(confPath);
+        } catch (error) {
+            vscode.window.showErrorMessage(error);
+        }
+    }
+
+    const filename = "global.json";
+
+    createConfigFile(rootPath, filename, new GlobalTemplate());
+
+}
+
+async function createSonarlintJson() {
+    const rootPath = vscode.workspace.rootPath;
+    const filename = "sonarlint.json";
+
+    createConfigFile(rootPath, filename, new SonarlintTemplate());
+}
+
+async function createConfigFile(rootPath: string, filename: string, template: configTemplate) {
+    if (!rootPath) {
+        return;
+    }
+    const filePath = path.join(rootPath, filename);
+    const action = "Open " + filename;
+    let selectedAction: string;
+
+    try {
+        await fs.stat(filePath);
+        selectedAction = await vscode.window.showInformationMessage(filename + " already exists", action);
+    } catch (error) {
+        try {
+            await fs.writeFile(filePath, JSON.stringify(template.getTemplateObject(), undefined, 4));
+            selectedAction = await vscode.window.showInformationMessage(filename + " was created", action);
+        } catch (error) {
+            vscode.window.showErrorMessage(error);
+        }
+    }
+
+    if (selectedAction) {
+        const textDocument = await vscode.workspace.openTextDocument(filePath);
+        await vscode.window.showTextDocument(textDocument);
+    }
 }
