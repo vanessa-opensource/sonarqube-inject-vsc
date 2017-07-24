d5737d5dfcf2d9131d27ab47f0d054625cf1b15e
Nikita Gryzlov <nixel2007@gmail.com>
2017-07-24 11:19:09 +0300

    Fix #14. Package sonarlint-cli with extension

---
+++ download-sonarlint-cli.js
@@ -0,0 +1,34 @@
+fs = require("fs-extra");
+extract = require("extract-zip");
+path = require("path");
+request = require("request");
+
+const pathToExtract = path.join(__filename, "./../tools");
+const pathToDownload = path.join(pathToExtract, "sonarlint-cli.zip");
+
+const sonarlintCLILocation =
+    "https://github.com/nixel2007/sonarlint-cli/releases/download/console-report-1.2/sonarlint-cli.zip";
+
+const options = {
+    url: sonarlintCLILocation,
+};
+
+console.log("Downloading of sonarlint-cli was started...");
+
+request(options)
+    .pipe(fs.createWriteStream(pathToDownload))
+    .on("finish", async () => {
+        await fs.remove(path.join(pathToExtract, "sonarlint-cli"));
+
+        extract(pathToDownload, { dir: pathToExtract }, async (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            await fs.chmod(path.join(pathToExtract, "sonarlint-cli", "bin", "sonarlint"), "755");
+            console.log("SonarLint was installed.");
+        });
+    })
+    .on("error", (err) => {
+        console.error(err);
+    });
